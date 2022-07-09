fba72b67bca059b0071d306f666ff556034d2fb2
sylimaaa <108972714+sylimaaa@users.noreply.github.com>
2022-07-09 13:50:26 +0700

    new site

---
+++ js/app.js
@@ -0,0 +1,75 @@
+$(function() {
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight();
+        scrollOffset = $(window).scrollTop();
+    
+
+    /* Header fixed */ 
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+
+    });
+
+    function checkScroll(scrollOffset) {
+        if( scrollOffset >= introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+
+    /* Smooth Scroll*/
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+        
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 300);
+    });
+
+
+    /* Menu nav toggle*/
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+
+
+    /*Collapse*/
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        
+        $(blockId).slideToggle();
+        $this.toggleClass("active");
+    });
+
+
+
+
+
+
+
+
+
+});
